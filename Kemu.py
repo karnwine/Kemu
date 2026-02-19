9c8f09b915b2e2c8fdffc4835195352d9b133a76
Arnwine <keith.arnwine@uster.com>
2026-02-19 11:08:23 -0500

    better partdict filter

--- Kemu.py
+++ Kemu.py
@@ -9,10 +9,11 @@ import TechTreeModding
 from Filepaths import Filepaths
 from Parts.Part import Part
 
+RED = "\033[91m"
+RESET = "\033[0m"
+
 def getLines(filepath):
     lines = []
-    RED = "\033[91m"
-    RESET = "\033[0m"
     try:
         with open(filepath, "r", encoding="UTF-8") as currentFile:
             for line in currentFile.readlines():
@@ -21,36 +22,42 @@ def getLines(filepath):
         print(f"{RED}ERROR: [{filepath}] not found.{RESET}")
     except UnicodeDecodeError:
         print(f"{RED}ERROR: Invalid file format.{RESET}")
+
     return lines
 
 def getCsvData(filepath):
-    RED = "\033[91m"
-    RESET = "\033[0m"
     if not Path(filepath).exists():
         print(f"{RED}ERROR: File \"{filepath}\" not found.{RESET}")
         sys.exit()
+
     csvData = []
     with open(filepath) as csvFile:
         reader = csv.reader(csvFile)
         for row in reader:
             csvData.append(row)
+
     return csvData
 
 def getPartDicts(partCfgFilepaths):
     partDicts = []
+
     for filepath in partCfgFilepaths:
         partDict = PartParser.getPartDict(getLines(filepath))
         if partDict == {}:
             continue
+
         if PartParser.getValueFromKey("TechRequired", partDict) == "Unresearcheable":
             continue
-        if PartParser.getValueFromKey("TechHidden", partDict) == "True":
+
+        val = PartParser.getValueFromKey("TechHidden", partDict)
+        if val and val.lower() == "true":
             continue
-        if PartParser.getValueFromKey("TechHidden", partDict) == "true":
-            continue
+
         if partDict.get("module") == None:
             continue
+
         partDicts.append(partDict)
+
     return partDicts
 
 def getParts(directoryName):
